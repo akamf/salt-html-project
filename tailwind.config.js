287767c9d49147e8af88e22cbf32934269871e5f
Andreas Kamf <andreaskamf@gmail.com>
2025-04-22 14:17:35 +0200

    removed fade animation that didnt work

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,7 @@
 module.exports = {
   content: ["./public/**/*.{html,js,ts}"],
   theme: {
-    extend: {
-      animation: {
-        fade: 'fadeIn 1s ease-out forwards',
-        'fade-out': 'fade-out 0.3s ease-in forwards',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
-        fadeOut: {
-          '0%': { opacity: '1' },
-          '100%': { opacity: '0' },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
