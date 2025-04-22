baf698202732415e6eed97d5f835bf53cbd454e3
Andreas Kamf <andreaskamf@gmail.com>
2025-04-22 10:28:33 +0200

    created index.html

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,7 @@ const button = document.getElementById('clickMe') as HTMLButtonElement | null;
 button?.addEventListener('click', () => {
   alert('Button clicked with TypeScript!');
 });
+
+function toggleMenu() {
+  document.getElementById('mobileMenu')?.classList.toggle('hidden');
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeIn 1s ease-out forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 }
